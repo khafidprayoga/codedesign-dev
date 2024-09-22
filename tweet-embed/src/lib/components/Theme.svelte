387726f7591c2dac8a3d2d1a13d3cd82840f2894
khafidprayoga <khafidp@pm.me>
2024-09-22 14:25:31 +0700

    adjust color on dark mode

--- tweet-embed/src/lib/components/Theme.svelte
+++ tweet-embed/src/lib/components/Theme.svelte
@@ -20,7 +20,7 @@
 <div class="flex items-center space-x-3 mx-auto px-3 py-1 rounded-full">
 	<button
 		on:click={toggleTheme}
-		class="p-2 rounded-full border border-gray-300 dark:border-gray-300 bg-twitter-blue dark:bg-twitter-dark-base hover:bg-gray-300 dark:hover:bg-gray-700 dark:bg-opacity-30 transition-colors duration-300"
+		class="p-2 rounded-full border border-gray-300 dark:border-gray-300 bg-twitter-blue dark:bg-twitter-dark-base hover:bg-gray-300 dark:hover:bg-twitter-blue transition-colors duration-100"
 		aria-label="Toggle dark mode"
 	>
 		{#if isDarkMode}
