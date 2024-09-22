d108a6bb4668b97fe6fe052907e7f52e9a7b2b8d
khafidprayoga <khafidp@pm.me>
2024-09-22 12:03:44 +0700

    add dark mode schema
    todo: implement dark toggle btn

--- tweet-embed/src/routes/+page.svelte
+++ tweet-embed/src/routes/+page.svelte
@@ -4,10 +4,10 @@
 	import { IconHeart, IconBrandTwitterFilled } from '@tabler/icons-svelte';
 
 	let likeCount: string = "1K";
-	let text = "Why do they call it 'debugging' when it feels more like 'wildly guessing and hoping for the best'? #programming #coding #debugging";
+	let text = `Why do they call it 'debugging' when it feels more like 'wildly guessing and hoping for the best'? #programming #coding #debugging`;
 
 	const getFormattedText = (text: String) => {
-	return text.replace(/#(\w+)/g, '<span class="text-blue-500 hover:underline hover:cursor-pointer">#$1</span>');
+	return text.replace(/#(\w+)/g, '<span class="text-blue-500 dark:twitter-text-hashtag hover:underline hover:cursor-pointer">#$1</span>');
 	};
 
 	let publishedAt = format(new Date(), "MMM D, YYYY");
@@ -18,12 +18,12 @@
 	class="
 flex flex-col
 w-1/2
-px-6 py-6
+px-8 py-6
 max-w-lg rounded overflow-hidden shadow-lg
 bg-white hover:shadow-xl transition-shadow duration-300
-"
->
-	<div id="tweet-head" class="flex flex-row mb-3">
+dark:bg-twitter-dark dark:text-white
+">
+	<div id="tweet-head" class="flex flex-row mb-0">
 		<div id="tweet-meta" class="flex flex-row w-1/2">
 			<img
 				class="rounded-full w-10 h-10 object-cover self-center ml-1 mr-1"
@@ -31,7 +31,7 @@ bg-white hover:shadow-xl transition-shadow duration-300
 				alt="tweet author profile"
 			/>
 			<div class="p-5">
-				<h3 id="tweet-author" class="py-1 uppercase">Jane Doe</h3>
+				<h3 id="tweet-author" class=" uppercase">Jane Doe</h3>
 				<p id="tweet-name">@realjanedoe</p>
 			</div>
 		</div>
@@ -44,7 +44,7 @@ bg-white hover:shadow-xl transition-shadow duration-300
 			{@html getFormattedText(text)}
 		</div>
 		<div id="tweet-action" class="mt-3 flex flex-row">
-			<IconHeart stroke={2} size="25" class="cursor-pointer hover:fill-gray-800"/>
+			<IconHeart stroke={2} size="25" class="cursor-pointer hover:fill-gray-800 dark:hover:fill-white"/>
 			<span id="like-count" class="ml-2">{likeCount}</span>
 			<span id="tweet-published-at" class="ml-2">{publishedAt}</span>
 		</div>
@@ -52,5 +52,4 @@ bg-white hover:shadow-xl transition-shadow duration-300
 </div>
 
 <style>
-
 </style>
--- tweet-embed/tailwind.config.js
+++ tweet-embed/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'selector',
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor:{
+        'twitter-blue': '#ECF8FF',
+        'twitter-dark': '#243447',
+      },
+      textColor:{
+        'twitter-hashtag': '#1DA1F2',
+      }
+    },
   },
   plugins: [],
 }
