255081a6ff2f8f41b647c6f0fc57797360caa334
khafidprayoga <khafidp@pm.me>
2024-09-25 21:43:33 +0700

    holadok: install tailwind and add navigation top section

---
+++ holadok/src/lib/components/Header.svelte
@@ -0,0 +1,77 @@
+<script>
+    import Logo from "$lib/assets/brand/logo.png";
+</script>
+
+
+<div class="navigation">
+    <div class="navigation-left">
+        <img src="{Logo}" alt="Holadok Brand Logo" >
+    </div>
+    <nav class="navigation-center">
+        <ul>
+            <li class="active">
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Features</a>
+            </li>
+            <li>
+                <a href="#">Blog</a>
+            </li>
+            <li>
+                <a href="#">About Us</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="navigation-right">
+        <span>
+            <a href="#">Login</a>
+        </span>
+        <span class="bordered">
+            <a href="#">Book Now</a>
+        </span>
+    </div>
+</div>
+
+<style lang='postcss'>
+    *  {
+        /* border: 1px solid red; */
+        @apply text-sm
+    }
+
+    .navigation {
+        @apply 
+        
+        flex w-full justify-between items-center absolute
+        py-2
+    }
+
+    .navigation-left img {
+        @apply w-full h-7
+    }
+
+    .navigation-center > ul , .navigation-right > span  {
+        @apply px-1 py-1
+    }
+    .navigation-center  ul {
+        @apply flex flex-row gap-4
+    }
+
+    .navigation-center  ul li:hover {
+        @apply underline
+    }
+
+
+    .navigation-center  ul li.active{
+        @apply font-semibold
+    }
+
+    .navigation-right {
+        @apply flex flex-row gap-4 font-semibold
+    }
+
+    .navigation-right span.bordered {
+        @apply border border-black rounded-sm
+    }
+</style>
--- holadok/src/routes/+page.svelte
+++ holadok/src/routes/+page.svelte
@@ -1,3 +1,13 @@
-<script>
-    import Background from "../lib/components/square.svelte";
+<script lang="ts">
+    import Header from "$lib/components/Header.svelte";
 </script>
+
+<div class="content">
+        <Header/>
+</div>
+
+<style lang="postcss">
+    .content {
+        /* @apply w-11; */
+    }
+</style>
