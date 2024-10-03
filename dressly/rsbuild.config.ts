026121cb97a45fb19b042041972420ee812cd459
khafidprayoga <khafidp@pm.me>
2024-10-03 11:50:03 +0700

    dressly: adjust rspack optimization

--- dressly/rsbuild.config.ts
+++ dressly/rsbuild.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginSvelte, PluginSvelteOptions } from '@rsbuild/plugin-svelte';
+import { pluginImageCompress } from "@rsbuild/plugin-image-compress";
 
 const svelteConfig: PluginSvelteOptions = {
   svelteLoaderOptions: {},
@@ -8,13 +9,25 @@ const svelteConfig: PluginSvelteOptions = {
   },
 };
 
+
 export default defineConfig({
-  plugins: [pluginSvelte(svelteConfig)],
+  plugins: [pluginSvelte(svelteConfig), pluginImageCompress(['png','svg'])],
   html: {
     title: 'DressLy - Discover the Latest Fashion Trends',
     favicon: './public/favicon.png',
-    meta:{
+    meta: {
       description: 'DressLy is a fashion discovery platform that helps you find the latest fashion trends and styles.',
     }
   },
+  performance:{
+    chunkSplit: {
+      strategy: 'split-by-experience',
+      forceSplitting: {
+        axios: /node_modules[\\/]axios/,
+      },
+    },
+  },
+  output:{
+    polyfill: 'usage',
+  }
 });
