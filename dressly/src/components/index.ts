954b58e329efc144ec9201691fa85da894bc4e8e
khafidprayoga <khafidp@pm.me>
2024-10-04 19:31:23 +0700

    dressly: fix import path err

--- dressly/src/components/index.ts
+++ dressly/src/components/index.ts
@@ -1,7 +1,7 @@
 import headerComponent from './Header/Header.svelte';
 import heroComponent from './Hero/Hero.svelte';
 import brandSliderComponent from './BrandSlider/BrandSlider.svelte';
-import newsletterComponent from './Newsletter/Newsletter.svelte';
+import newsletterComponent from './NewsLetter/NewsLetter.svelte';
 import footerComponent from "./Footer/Footer.svelte";
 
 export {
